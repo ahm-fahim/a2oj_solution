e041b77310e4757ef8a393116a8da2f4274c90a6
ahm-fahim <contactahmfahim@gmail.com>
2023-12-15 16:38:02 +0600

    a2oj solution

---
+++ 2_Beautiful_Matrix.cpp
@@ -0,0 +1,63 @@
+
+/****~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~****\
+ |  Problems! Problems! Problems! I Don't Like It, I Avoid.             |
+ |  But Problems Likes Me! I can't Avoid!                               |
+ |                                                                      |
+ |  A.H.M. Fahim Morshed                                                |
+ |                                                                      |
+ |  ```Is a competitive programming enthusiast who is deeply            |
+ |  passionate about coding. Hailing from Bogura, Bangladesh,           |
+ |  his coding journey started at a young age, driven by a              |
+ |  fascination for solving intricate problems.                         |
+ |                                                                      |
+ |  Fahim has excelled in competitive programming, consistently         |
+ |  ranking among the top participants in various                       |
+ |  contests.Beyond competitions, he shares his                         |
+ |  expertise by mentoring and assisting fellow                         |
+ |  programmers on online forums.                                       |
+ |                                                                      |
+ |  His dedication to self-improvement, willingness to embrace new      |
+ |  challenges, and effective teamwork make him a valuable              |
+ |  asset to any project.                                               |
+ |                                                                      |
+ |  Fahim's love for coding, combined with his diverse interests,       |
+ |  reflects his well-rounded personality, making him an inspiring      |
+ |  figure in the programming community at Southeast University.```     |
+ |                                                                      |
+ |  Github   - ahm-fahim                                                |
+ |  Linkedin - https://www.linkedin.com/in/md-fahim-morshed-5b2126233/  |
+ |  Gmail    - contactahmfahim@gmail.com                                |
+\****~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~****/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[5][5];
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    int x, y;
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            if (a[i][j] == 1)
+            {
+                x = i;
+                y = j;
+            }
+        }
+    }
+
+    cout << abs(3 - x) + abs(3 - y) << '\n';
+
+    return 0;
+}
